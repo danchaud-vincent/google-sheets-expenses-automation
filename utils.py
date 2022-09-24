ef230a8e91298c9ceeaabf7f80f19183a618434e
danchaud-vincent <vdanchaud@gmail.com>
2022-09-24 12:04:29 +0200

    remove personal expenses file

--- utils.py
+++ utils.py
@@ -4,3 +4,8 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+import os
+
+# scopes
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
